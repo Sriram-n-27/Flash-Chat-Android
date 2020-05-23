a2f07346949ebeea54bc35e8c97f0824aac4ccb9
Sriram N <43564446+Sriram-n-27@users.noreply.github.com>
2020-05-23 17:18:39 +0530

    First commit

---
+++ app/src/main/java/com/example/flashchat/ChatListAdapter.java
@@ -0,0 +1,119 @@
+package com.example.flashchat;
+
+import android.app.Activity;
+import android.content.Context;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+
+
+
+public class ChatListAdapter extends BaseAdapter {
+
+    private Activity mActivity;
+    private DatabaseReference mDatabaseReference;
+    private String mDisplayName;
+    private ArrayList<DataSnapshot> mSnapshotList;
+
+    private ChildEventListener mListener = new ChildEventListener() {
+        @Override
+        public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+            mSnapshotList.add(dataSnapshot);
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+        }
+
+        @Override
+        public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+        }
+
+        @Override
+        public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+    };
+
+    public ChatListAdapter(Activity activity, DatabaseReference ref, String name) {
+        mActivity = activity;
+        mDatabaseReference = ref.child("messages");
+        mDisplayName = name;
+        mSnapshotList = new ArrayList<>();
+
+        mDatabaseReference.addChildEventListener(mListener);
+    }
+
+    static class ViewHolder {
+        TextView authorName;
+        TextView body;
+        LinearLayout.LayoutParams params;
+    }
+
+    @Override
+    public int getCount() {
+
+        return mSnapshotList.size();
+    }
+
+    @Override
+    public InstantMessage getItem(int i) {
+        DataSnapshot snapshot = mSnapshotList.get(i);
+        return snapshot.getValue(InstantMessage.class);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        if(view == null) {
+            LayoutInflater inflater = (LayoutInflater) mActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.chat_msg_row,viewGroup,false);
+
+            final ViewHolder holder = new ViewHolder();
+            holder.authorName = (TextView) view.findViewById(R.id.author);
+            holder.body = (TextView) view.findViewById(R.id.message);
+            holder.params = (LinearLayout.LayoutParams) holder.authorName.getLayoutParams();
+            view.setTag(holder);
+        }
+
+        final InstantMessage message = getItem(i);
+        final ViewHolder holder = (ViewHolder) view.getTag();
+
+        String author = message.getAuthor();
+        holder.authorName.setText(author);
+
+        String msg = message.getMessage();
+        holder.body.setText(msg);
+        return view;
+    }
+
+    public void cleanup(){
+        mDatabaseReference.removeEventListener(mListener);
+    }
+}
+
---
+++ app/src/main/java/com/example/flashchat/LoginActivity.java
@@ -0,0 +1,94 @@
+package com.example.flashchat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    // TODO: Add member variables here:
+    // UI references.
+    private AutoCompleteTextView mEmailView;
+    private EditText mPasswordView;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.login_email);
+        mPasswordView = (EditText) findViewById(R.id.login_password);
+
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.integer.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        // TODO: Grab an instance of FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    // Executed when Sign in button pressed
+    public void signInExistingUser(View v)   {
+        // TODO: Call attemptLogin() here
+        attemptLogin();
+    }
+
+    // Executed when Register button pressed
+    public void registerNewUser(View v) {
+        Intent intent = new Intent(this, com.example.flashchat.RegisterActivity.class);
+        finish();
+        startActivity(intent);
+    }
+
+    // TODO: Complete the attemptLogin() method
+    private void attemptLogin() {
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+        Toast.makeText(this,"Login in progress !!!",Toast.LENGTH_SHORT).show();
+
+        // TODO: Use FirebaseAuth to sign in with email & password
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d("Flashchat", "Login oncreate : " + task.isSuccessful());
+                Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                if (!task.isSuccessful()) {
+                    Log.d("Flashchat", "Login oncreate : failed ");
+                } else {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    finish();
+                    startActivity(intent);
+                }
+            }
+        });
+
+    }
+    // TODO: Show error on screen with an alert dialog
+
+}
